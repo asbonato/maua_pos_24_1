ec8ff4923c4e1e638d45ee46f4810008447f12f2
asbonato <asbonato@gmail.com>
2024-04-05 11:41:58 -0300

    fix(backend): remoção do subreposiório git

---
+++ intro_git_calc/calc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  soma: (x, y) => x + y,
+  subtracao: function(x, y){
+    return x - y
+  },
+  multiplicacao: (x, y) => {
+    return x * y
+  },
+  divisao: function (x, y){
+    return x / y
+  }
+}
+
